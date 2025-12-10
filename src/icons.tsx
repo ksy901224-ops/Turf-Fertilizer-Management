4eea498bd755e777e7deecbb932ddb3b93b3f4e3
ksy901224-ops <ksy901224@gmail.com>
2025-12-11 03:15:22 +0900

    refactor: Update CalculatorIcon props and LogEntry definition
    The `CalculatorIcon` component now accepts a `className` prop to allow for more flexible styling.
    The `LogEntry` interface definition was cleaned up.

--- src/icons.tsx
+++ src/icons.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 export const ChatIcon = () => (
@@ -54,8 +55,8 @@ export const SparklesIcon = () => (
     </svg>
 );
 
-export const CalculatorIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+export const CalculatorIcon = ({ className = "h-6 w-6" }: { className?: string; }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
         <path strokeLinecap="round" strokeLinejoin="round" d="M9 7h6m-6 4h6m-6 4h6m2 4H7a2 2 0 01-2-2V7a2 2 0 012-2h10a2 2 0 012 2v10a2 2 0 01-2 2z" />
     </svg>
 );
